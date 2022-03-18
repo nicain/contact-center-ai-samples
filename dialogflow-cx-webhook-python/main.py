8c8a2df045e47f39ba02cba82bc70b44d93ef761
Nicholas Cain <nicholascain@google.com>
2022-03-18 10:26:02 -0700

    Small change to test actions

--- dialogflow-cx-webhook-python/main.py
+++ dialogflow-cx-webhook-python/main.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""setup.py calls the DIalogflow API to setup a webhook sample"""
+"""setup.py calls the DIalogflow API to setup a webhook sample."""
 
 import argparse
 import time
