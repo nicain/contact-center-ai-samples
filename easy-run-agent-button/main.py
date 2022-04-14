e2c92bc030ae66f0ab4e404ec0c9456f54193a6a
Gal Zahavi <38544478+galz10@users.noreply.github.com>
2022-04-14 10:26:22 -0700

    added print debugging

--- easy-run-agent-button/main.py
+++ easy-run-agent-button/main.py
@@ -32,6 +32,13 @@ def create_agent():
 
     created_agent = client.create_agent(request=request)
     agent_name = created_agent.name
+    agent_split = agent.split('/')
+
+    agent_id = agent_split[5]
+
+    agent_url = f"https://dialogflow.cloud.google.com/cx/projects/{project_id}/locations/global/agents/{agent_id}/flows/00000000-0000-0000-0000-000000000000/flow_creation"
+
+    print(f"export AGENT_URL='{agent_url}'")
 
     print("Restoring Agent")
     sample_restore_agent(agent_name)
@@ -43,7 +50,7 @@ def sample_restore_agent(agent_name):
 
     # Initialize request argument(s)
     request = dialogflowcx_v3.RestoreAgentRequest(
-        agent_uri="gs://testingeasyagent/agent1",
+        agent_uri="gs://testingeasyagent/travel",
         name=agent_name,
     )
 
